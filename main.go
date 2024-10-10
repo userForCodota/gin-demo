059249296d4caf13781452d4bcd4107461441629
gust <3065980942@qq.com>
2024-10-10 23:29:14 +0800

    简单的GET请求 & favicon.ico

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/favicon"
+)
+
+func main() {
+	// 创建一个服务
+	ginServer := gin.Default()
+
+	// 图标
+	ginServer.Use(favicon.New("./favicon.ico"))
+
+	// 注册一个路由
+	ginServer.GET("/hello", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+
+	//	服务器监听端口
+	ginServer.Run(":8082")
+
+}
